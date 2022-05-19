885f3c0088b514315bedaafa8dc0c17ab4023464
jacobwilliams-it <104857712+jacobwilliams-it@users.noreply.github.com>
2022-05-19 14:41:45 -0500

    comment

---
+++ flask-01-02-hello-world-app-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def head():
+    return "<h1>Hello World</h1>"
+@app.route('/second')
+def second():
+    return "This is the second page"
+@app.route('/third/subthird')
+def third():
+    return "This is the subpage of third page"
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
+if __name__ == '__main__':
+    # app.run(debug=False)
+    app.run(port=80, debug=False)
