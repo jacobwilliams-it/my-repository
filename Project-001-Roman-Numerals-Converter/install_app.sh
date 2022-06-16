767f46de50a2f11efbe3f6006bce1608ea2b4d3e
jacobwilliams-it <104857712+jacobwilliams-it@users.noreply.github.com>
2022-06-16 13:14:51 -0500

    Roman Numeral Project

---
+++ Project-001-Roman-Numerals-Converter/install_app.sh
@@ -0,0 +1,22 @@
+!#/bin/bash -x
+
+sudo yum update -y
+
+sudo yum install python3 -y
+
+sudo pip3 install flask
+
+cd /home/ec2-user
+
+FOLDER='https://raw.githubusercontent.com/jacobwilliams-it/my-repository/main/Project-001-Roman-Numerals-Converter'
+
+wget ${FOLDER}/app.py
+
+mkdir templates && cd templates
+
+wget ${FOLDER}/index.html
+
+wget ${FOLDER}/result.html
+
+cd ..
+sudo python3 app.py
