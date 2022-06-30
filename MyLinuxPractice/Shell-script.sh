200cc5a10e8b4529822a61555f30ea0405eb22f2
jacobwilliams-it <104857712+jacobwilliams-it@users.noreply.github.com>
2022-06-30 18:07:35 -0500

    Added practice folder for Linux

---
+++ MyLinuxPractice/Shell-script.sh
@@ -0,0 +1,67 @@
+#Scripts should always begin with #!/bin/bash
+#!/bin/bash  
+
+# echo Hello World!
+
+# echo "Hello World"
+
+# VARIABLES
+# Variables are uppercase by convention
+# Variables should be Letters, numbers or underscores
+
+# NAME="Micah"
+#   echo "My name is $NAME"
+#   echo "My name is ${NAME}"
+
+# USER-INPUT
+#  read -p "Enter your name: " NAME
+#   echo "Hello $NAME, nice to meet you!"
+
+
+# Simple if statement
+# if [  "$NAME" == "Micah" ]
+# then echo "Your name is Micah"
+# fi
+
+# IF-ELSE
+# if [  "$NAME" == "Micah" ]
+# then 
+#   echo "Your name is Micah"
+# else
+#    echo "Your name is NOT Micah"
+
+# fi
+
+#ELSE-IF (elif)
+# if [  "$NAME" == "Micah" ]
+# then 
+#     echo "Your name is Micah"
+# elif [  "$NAME" == "Jacob"  ]
+# then
+#     echo "Your name is Jacob"
+# else
+#     echo "Your name is not familiar"
+
+# fi
+
+
+# COMPARISON
+
+NUM1=31
+NUM2=4
+if [ " $NUM1" -gt "$NUM2" ]
+then
+    echo "$NUM1 is greater than $NUM2"
+else 
+    echo "$NUM1 is less than $NUM2"
+fi
+
+######
+# -eq is used to determine if values are equal
+# -ne is used to determine if values are not equal
+# -gt is used for greater than
+# -ge is used for greater than or equal to
+# -lt is less than
+# -le is less than or equal to
+#######
+
