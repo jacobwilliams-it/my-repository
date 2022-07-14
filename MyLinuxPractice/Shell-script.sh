c5b8dae5ab80414da60c520b60fcc4801d1c774d
jacobwilliams-it <104857712+jacobwilliams-it@users.noreply.github.com>
2022-07-14 15:33:40 -0500

    Cloudformation Template for kittens carousel static website deploy on s3 through     cloudfront and route 53

--- MyLinuxPractice/Shell-script.sh
+++ MyLinuxPractice/Shell-script.sh
@@ -97,3 +97,24 @@
 # *)
 #     echo "Please enter Yes or No"
 # esac
+
+!/bin/bash
+
+read -p "Hey, Whats your name? " NAME
+echo "hello $NAME"
+
+read -p "Are you a boy or a girl? " ANSWER
+
+if [ $ANSWER == "boy" ]
+
+then
+    echo "You will be a man one day, $NAME"
+
+elif [ $ANSWER == "girl" ]
+
+then
+    echo "You will be a woman one day, $NAME"
+
+fi
+
+
